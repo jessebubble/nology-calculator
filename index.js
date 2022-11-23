7237e786a945a63d745f9feb6ac793b69320943b
Jesse Hernandez <jessebubble@jesses-air.attlocal.net>
2022-11-23 17:10:25 -0600

    dom manipulation plus  math symbol buttons updated

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const display = document.querySelector(".calculator-display");
 const buttons = Array.from(document.querySelectorAll(".calculator-buttons"));
+const percent = document.querySelector("#percent");
+const plusminus = document.querySelector("#plusminus");
+const divide = document.querySelector("#divide");
+const multiply = document.querySelector("#multiply");
 
 buttons.forEach((button) => {
     button.addEventListener("click", (e) => {
@@ -7,7 +11,6 @@ buttons.forEach((button) => {
     console.log(e);
     console.log(e.target);
     console.log(e.target.innerText); */
-
     const buttonValue = e.target.innerText;
     const displayValue = display.innerText;
 
@@ -17,32 +20,54 @@ buttons.forEach((button) => {
     else if (buttonValue === '=') {
         display.innerText = window.Function(`return ${displayValue}`)();
     }
+    else if (buttonValue === 'x') {
+        display.innerText = displayValue + '*';
+    }
+    else if (buttonValue === '÷') {
+        display.innerText = displayValue + '/';
+    }
+    else if (buttonValue === '%') {
+        display.innerText = displayValue + '/100';
+    }
+    else if (buttonValue === '+/-') {
+        display.innerText = displayValue + '*-1';
+    }
     else {
         display.innerText = displayValue + buttonValue;
     }
-
     });
 });
 
+const logo = document.querySelector(".navbar-logo");
+const navbarlinks = Array.from(document.querySelectorAll(".navbar-links"));
+const herotitle = document.querySelector(".hero-title");
 
+logo.addEventListener("click", () => {
+    navbarlinks.forEach((link) => {
+        link.innerHTML = 'DOM Manipulation';
+        link.style.color = 'red';
+        link.style.fontSize = '30px';
+        link.style.fontWeight = 'bold';
+        link.style.transition = 'all 1s ease-in-out';
+        link.style.transform = 'rotate(360deg)';
+    });
+});
+herotitle.addEventListener("click", () => {
+    herotitle.innerHTML = 'Manipulate the DOM today!';
+    herotitle.style.color = 'yellow';
+    herotitle.style.fontSize = '36px';
+    herotitle.style.fontWeight = 'bold';
+    herotitle.style.transition = 'all 1s ease-in-out';
+    herotitle.style.scale = '1';
+});
 
+const heroicon = document.querySelectorAll(".hero-icon");
+const herocalculator = document.querySelector(".hero-calculator");
+const images = ["./assets/bg1.jpg", "./assets/bg2.jpg", "./assets/bg3.jpg", "./assets/bg4.jpg", "./assets/bg5.jpg"];
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+heroicon.forEach((icon) => {
+    icon.addEventListener("click", () => {
+        herocalculator.style.backgroundImage = `url(${images[Math.floor(Math.random() * images.length)]})`;
+        herocalculator.style.transition = 'all 1s ease-in-out';
+    });
+});
